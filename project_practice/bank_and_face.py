dd60b613d4c7f5bd98bdde1b9a164e353e062750
jejo0418 <2539724134@qq.com>
2023-08-29 00:23:25 +0800

    第一次提交

---
+++ project_practice/bank_and_face.py
@@ -0,0 +1,55 @@
+import requests
+import json
+import base64
+import cv2
+import numpy as np
+
+a1 = 'G:/python project/project_practice/face/1.jpg'
+a2 = 'G:/python project/project_practice/face/2.JPG'
+a3 = 'G:/python project/project_practice/face/3.JPG'
+faces = {'9559980210373015416': a1, '6224211100235159200': a2, '9559980461946691213': a3}
+informations = {a1: ['郑秀妍', '15108220418'], a2: ['李晗', '15103220404'], a3: ['王笙', '15108211818']}
+
+def card_recognition(img_path):
+    http_url = 'https://api-cn.faceplusplus.com/cardpp/v1/ocrbankcard'
+    data = {"api_key": 'xo87FRq-A2JSd0OEjCm-ayclcIShyiI1',
+            "api_secret": 'Qe8K8p8RAJ57b13TtzXKaeiHPOyUyyzq',
+                }
+    files = {"image_file": open(img_path, "rb")}
+    response = requests.post(http_url, data=data, files=files)
+    print(response.text)
+    data = json.loads(response.text)
+    attr = data['bank_cards'][0]['number']
+    print(attr)
+    return attr
+
+def person_beauty(img_path):
+    http_url = 'https://api-cn.faceplusplus.com/facepp/v1/beautify'
+    data = {"api_key": 'xo87FRq-A2JSd0OEjCm-ayclcIShyiI1',
+            "api_secret": 'Qe8K8p8RAJ57b13TtzXKaeiHPOyUyyzq',
+            "whitening": 100,
+            "smoothing": 100
+                }
+    files = {"image_file": open(img_path, "rb")}
+    response = requests.post(http_url, data=data, files=files)
+    print(response.text)
+    data = json.loads(response.text)
+    img_data = base64.b64decode(data['result'])
+    img_array = np.fromstring(img_data, np.uint8)
+    img = cv2.imdecode(img_array, cv2.COLOR_RGB2BGR)
+    return img
+
+
+
+if __name__ == "__main__":
+    b = card_recognition('G:/python project/project_practice/bank_card/3.jpg')
+    img = faces[b]
+    name = informations[img][0]
+    person_number = informations[img][1]
+    img = person_beauty(img)
+    text = "name:{0},phone_number:{1}".format(name, person_number)
+    cv2.putText(img, text, (30, 40), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (255, 0, 0), 2)
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+
+
---
+++ project_practice/cv_camera.py
@@ -0,0 +1,122 @@
+import sys
+import cv2
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QFileDialog, QMainWindow
+from ui_mainwindow import Ui_MainWindow
+import bank_and_face
+
+
+class PyQtMainEntry(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.filename = 'G:/python project/project_practice/result/1.jpg'
+        self.filename2 = 'G:/python project/project_practice/result/2.jpg'
+        self.camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+        self.is_camera_opened = False  # 摄像头有没有打开标记
+
+        # 定时器：30ms捕获一帧
+        self._timer = QtCore.QTimer(self)
+        self._timer.timeout.connect(self._queryFrame)
+        self._timer.setInterval(30)
+
+    def btnOpenCamera_Clicked(self):
+        '''
+        打开和关闭摄像头
+        '''
+        self.is_camera_opened = ~self.is_camera_opened
+        if self.is_camera_opened:
+            self.label.setText('关闭摄像头')
+            self._timer.start()
+        else:
+            self.label.setText('打开摄像头')
+            self._timer.stop()
+
+    def btnCapture_Clicked(self):
+        '''
+        捕获图片,此位置保存图片，下面传到face++
+        '''
+        # 摄像头未打开，不执行任何操作
+        if not self.is_camera_opened:
+            return
+
+        self.captured = self.frame
+        print(self.frame)
+        cv2.imwrite(self.filename, self.frame)
+        # 后面这几行代码几乎都一样，可以尝试封装成一个函数
+        rows, cols, channels = self.captured.shape
+        bytesPerLine = channels * cols
+        # Qt显示图片时，需要先转换成QImgage类型
+        QImg = QImage(self.captured.data, cols, rows, bytesPerLine, QImage.Format_RGB888)
+        self.labelCapture.setPixmap(QPixmap.fromImage(QImg).scaled(
+            self.labelCapture.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+    def btnReadImage_Clicked(self):
+        '''
+        从本地读取图片 文件路径不能有中文
+        '''
+        # 打开文件选取对话框
+        filename, _ = QFileDialog.getOpenFileName(self, '打开图片')
+        if filename:
+            self.captured = cv2.imread(str(filename))
+            cv2.imwrite(self.filename, self.captured)
+            print(self.captured)
+            # OpenCV图像以BGR通道存储，显示时需要从BGR转到RGB
+            self.captured = cv2.cvtColor(self.captured, cv2.COLOR_BGR2RGB)
+
+            rows, cols, channels = self.captured.shape
+            bytesPerLine = channels * cols
+            QImg = QImage(self.captured.data, cols, rows, bytesPerLine, QImage.Format_RGB888)
+            self.labelCapture.setPixmap(QPixmap.fromImage(QImg).scaled(
+                self.labelCapture.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+    def btnGray_Clicked(self):
+
+        # 如果没有捕获图片，则不执行操作
+        # if not hasattr(self,"captured"):
+            # return
+        # self.img = self.captured
+        try:
+            bank_card = bank_and_face.card_recognition(self.filename)
+            img = bank_and_face.faces[bank_card]
+            name = bank_and_face.informations[img][0]
+            person_number = bank_and_face.informations[img][1]
+            self.information_name.setText(name)
+            self.information_num.setText(person_number)
+            img = bank_and_face.person_beauty(img)
+            cv2.imwrite(self.filename2, img)
+            self.captured = cv2.imread(str(self.filename2))
+            # OpenCV图像以BGR通道存储，显示时需要从BGR转到RGB
+            self.captured = cv2.cvtColor(self.captured, cv2.COLOR_BGR2RGB)
+            rows, cols, channels = self.captured.shape
+            bytesPerLine = channels * cols
+            QImg = QImage(self.captured.data, cols, rows, bytesPerLine, QImage.Format_RGB888)
+            self.labelResult.setPixmap(QPixmap.fromImage(QImg).scaled(
+                self.labelResult.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+        except:
+            self.labelResult.setText("请重试")
+
+
+
+    @QtCore.pyqtSlot()
+    def _queryFrame(self):
+        '''
+        循环捕获图片'''
+        ret, self.frame = self.camera.read()
+        img_rows, img_cols, channels = self.frame.shape
+        bytesPerLine = channels * img_cols
+
+        cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB, self.frame)
+
+        QImg = QImage(self.frame.data, img_cols, img_rows, bytesPerLine,QImage.Format_RGB888)
+        self.labelCamera.setPixmap(QPixmap.fromImage(QImg).scaled(
+            self.labelCamera.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = PyQtMainEntry()
+    window.show()
+    sys.exit(app.exec_())
